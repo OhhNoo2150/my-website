8858764babf5727d6a71c001eba29d2d42e42325
Daniel Rivera <daniel.i.rivera@protonmail.com>
2022-06-09 13:27:44 -0500

    updating site pages, styling. need to fix sections

--- src/components/puertoRico/PuertoRico.jsx
+++ src/components/puertoRico/PuertoRico.jsx
@@ -5,12 +5,14 @@ export default function PuertoRico() {
     <div className="puertoRico" id="puertoRico">
       <div className="left">
         <div className="container">
+          <h1>Bienvenidos</h1>
+          <h5>Some information on my beautiful Island</h5>
 
         </div>
       </div>
       <div className="right">
         <div className="container">
-
+          <img src="../1stBn5thMarLogo.jpeg" alt="1/5" />
         </div>
       </div>
     </div>
